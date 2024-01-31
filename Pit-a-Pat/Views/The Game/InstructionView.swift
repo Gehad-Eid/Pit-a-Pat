abb602cf925983fc066270ebf42a18bee5cd2141
gehad1995 <gehad.gehad1995@gmail.com>
2024-01-31 12:58:02 +0300

    debug

---
+++ Pit-a-Pat/Views/The Game/InstructionView.swift
@@ -0,0 +1,18 @@
+//
+//  InstructionView.swift
+//  Pit-a-Pat
+//
+//  Created by Gehad Eid on 31/01/2024.
+//
+
+import SwiftUI
+
+struct InstructionView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    InstructionView()
+}
