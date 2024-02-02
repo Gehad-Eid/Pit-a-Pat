273ea5487a7be6712e102d1615ddcbc105b1f829
fayzah <ffowz110@hotmail.com>
2024-02-02 06:29:16 +0300

    a

---
+++ Pit-a-Pat/Views/AddFriendsView.swift
@@ -0,0 +1,19 @@
+//
+//  AddFriendsView.swift
+//  Pit-a-Pat
+//
+//  Created by Faizah Almalki on 21/07/1445 AH.
+//
+
+import SwiftUI
+
+struct AddFriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFriendsView()
+}
+
